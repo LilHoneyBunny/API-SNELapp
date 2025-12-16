f28ce359e821d46f512cb429db3ff20e53eb687c
MiriamRZ03 <miriam28.rz@gmail.com>
2025-12-15 19:08:17 -0600

    Añadiendo api gateway

--- MINAO-ApiGateway/src/index.js
+++ MINAO-ApiGateway/src/index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', require('./routes/users.routes'));
-app.use('/quizzes', require('./routes/quizzes.routes'));
-app.use('/courses', require('./routes/courses.routes'));
+app.use('/minao_systems/users', require('./routes/users.routes'));
+app.use('/minao_systems/quizzes', require('./routes/quizzes.routes'));
+app.use('/minao_systems/courses', require('./routes/courses.routes'));
 
 app.listen(3002, () => {
   console.log('API Gateway running on port 3002');
