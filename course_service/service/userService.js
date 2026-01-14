4c4951d7201b1d53fc7a548203db05230294a428
EdmundoRdM <edmundoramos28@hotmail.com>
2026-01-14 10:46:13 -0600

    salida de metodos para reporte
    Se hizo el parseo de puertos para que salieran al gateway

--- course_service/service/userService.js
+++ course_service/service/userService.js
@@ -1,20 +1,22 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const getStudentsInfoFromUserService = async (studentIds) => {
-    if (!studentIds || studentIds.length === 0) return [];
+  if (!studentIds || studentIds.length === 0) return [];
 
-    const url = `http://localhost:3000/minao_systems/users?ids=${studentIds.join(',')}`;
+  const url = `http://users_service:3000/minao_systems/users?ids=${studentIds.join(',')}`;
 
-    try {
-        const res = await fetch(url);
-        if (!res.ok) throw new Error(`Error fetching users: ${res.statusText}`);
-        const data = await res.json();
-        return data.students; 
-    } catch (err) {
-        console.error("Error getting students info from user service:", err);
-        return studentIds.map(id => ({ studentId: id, name: "Desconocido", average: 0 }));
-    }
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.students;
+  } catch (err) {
+    console.error("Error getting students info:", err);
+    return studentIds.map(id => ({ studentId: id, name: "Desconocido", average: 0 }));
+  }
 };
+
+
+
 
 const getInstructorInfoFromUserService = async (instructorId) => {
     if (!instructorId || instructorId.length === 0) return [];
